88556634f24da2d5dad5d5482e1fdcd3b056c953
brian <brian@grug.dev>
2023-11-18 00:55:35 -0500

    Better error handling

---
+++ components/Error.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+
+export default function Error() {
+
+    const router = useRouter();
+    const handleRefresh = () => {
+        router.refresh();
+    };
+
+    return (
+        <div className='flex justify-center'>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Error</CardTitle>
+                    <CardDescription>There was an error fetching your data</CardDescription>
+                </CardHeader>
+                <CardFooter className="flex justify-center">
+                    <Button onClick={handleRefresh}>Retry</Button>
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
